51cf52c560c67f0b4bdb535f6a7861f7e4716d8c
leejunho210 <163738398+leejunho210@users.noreply.github.com>
2024-09-23 05:21:14 +0900

    변수, Datatype [문자열, 숫자형(숫자형의 사칙연산), 리스트(리스트의 인덱싱과 슬라이싱), 딕셔너리, 불린]
    변수 : 프로그래밍에서 데이터를 저장하고 조작하기 위한 요소 (오른쪽에서 왼쪽으로 소스코드를 입력) 세로운 변수 할당 시 ' =(assignment) ' 기호를 사용
    
    문자열 : 기본 문자들의 시퀀스, 문자열은 작은 따옴표 (') 또는 큰 따옴표(")로 둘러 쌓임 - 작은 따옴표와 큰 따옴표를 구분해서 사용하는 이유? = 따옴표를 문자열 데이터로 사용할 때도 있어서, 문자열 연산 가능 + - *, 문자열 길이 구하는것도 가능 len()함수
    문자열의 인덱싱(각 문자열 마다 번호(순서)를 부여, 컴퓨터는 시작 숫자를 '1'이 아닌 '0'으로 부여)과 슬라이싱(추출하고자 하는 문자열의 인덱싱 번호를 통해 호출)
    
    숫자형 : 사칙연산 (+ - * / ), 제곱(**), 나머지(%), 몫 계산자(//)
    
    리스트 : 여러 값을 저장하는 배열, 리스트에 포함된 값은 요소, 대괄호로 표현 요소는 쉼표로 구분
    
    리스트의 인덱싱(리스트의 각 요소 마다 번호를 부여)과 슬라이싱(추출하고자 하는 리스트의 인덱싱 번호를 통해 호출)
    리스트의 요소 개수 확인 len()함수 
    리스트에 요소 추가 append() / 마지막
    리스트 요소 삭제 remove()
    
    딕셔너리 : 자료형은 키-값(key-value) 쌍을 저장하는 데이터 타입, 중괄호 '{}'로 둘러 쌓여 있으며, 각 키와 값은 ' : '콜론으로 구분
    
    딕셔너리 호출은 '키(key)' 값을 사용하여 키에 해당하는 '값(value)'를 호출
    
    딕셔너리 값 변경은 딕셔너리의 키를 통해 해당 변수 호출 후 새로운 값을 할당
    
    불린 : 참(true) 또는 거짓 (false) 값을 나타내는 데이터 타입, 조건식 또는 제어문에서 특정 조건이 참(true)일 떄 특정 동작을 가능하게 만들 때 사용

---
+++ 3주.py
@@ -0,0 +1,12 @@
+hello = 'Hello, World!'
+print(hello[0:5])
+
+result = (7+3)**2
+print(result)
+
+A = [1, 2, 3, 4, 5]
+Result = A[-1:-6:-1]
+print(Result)
+
+Dictionary = {'apple': 3, 'banana': 5, 'cherry': 2}
+print(Dictionary['apple'])
